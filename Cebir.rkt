b6dffd7f15a98d3c007db3ad81ac6384334ffc78
chrisstephenson <cs@chrisstephenson.com>
2024-06-06 13:21:24 +0300

    All existing files added

---
+++ Cebir.rkt
@@ -0,0 +1,27 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Cebir) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require "Teachpacks/fonksiyon-teachpack.rkt")
+;; should have the graphivc teachpack as well
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; #1 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Giren değeri beş artan bir fankiyon
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; #2 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Giren değeri 6 ile çarpan bir fonksiyon
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; #3 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Giren değeri 6 ile çarptıktan sonra 5 ekleyen bir fonksiyon
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; #4 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; height isimli ve giren v değeri 4 ile çarpıp 60 ekleyen bir fonksiyon
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; #5 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; distance isimli ve giren t değerinin karesini alıp 10 ekleyen bir fonksiyon
+
+
---
+++ Roket.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Roket) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require "Teachpacks/fonksiyon-teachpack.ss")
+
+
+(define (roket-yüksekliği saniye) 0)
+
+
+;;;; "Run" tıklanınca aşağıdan girilen bu satır roketin uçmasını sağlayacak
+;;;; Saniyeleri ilerletmek için boşluk tuşuna basın
+(başlat roket-yüksekliği)
+
+
+;; Bunu çalıştırdıktan sonra "başlat" yerine "uzay" "grafik" yada "hepsi" deneyebilirsin! 
